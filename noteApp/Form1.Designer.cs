8e3f6528c9af31d911d1926e52d14a22780e1656
Szénási Kristóf <Sznsi Kristf@DESKTOP-VIGI9V5>
2023-01-25 19:46:27 +0100

    Add project files.

---
+++ noteApp/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace noteApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.TxtMessage = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvNote = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNote)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(138, 38);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(340, 34);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // TxtMessage
+            // 
+            this.TxtMessage.Location = new System.Drawing.Point(138, 121);
+            this.TxtMessage.Multiline = true;
+            this.TxtMessage.Name = "TxtMessage";
+            this.TxtMessage.Size = new System.Drawing.Size(340, 240);
+            this.TxtMessage.TabIndex = 1;
+            // 
+            // btnNew
+            // 
+            this.btnNew.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnNew.FlatAppearance.BorderSize = 0;
+            this.btnNew.ForeColor = System.Drawing.Color.Snow;
+            this.btnNew.Location = new System.Drawing.Point(12, 400);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(185, 40);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = false;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.ForeColor = System.Drawing.Color.Snow;
+            this.btnSave.Location = new System.Drawing.Point(263, 400);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(185, 40);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnRead.FlatAppearance.BorderSize = 0;
+            this.btnRead.ForeColor = System.Drawing.Color.Snow;
+            this.btnRead.Location = new System.Drawing.Point(514, 400);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(185, 40);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Crimson;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.ForeColor = System.Drawing.Color.Snow;
+            this.btnDelete.Location = new System.Drawing.Point(765, 400);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(185, 40);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvNote
+            // 
+            this.dgvNote.AllowUserToAddRows = false;
+            this.dgvNote.AllowUserToDeleteRows = false;
+            this.dgvNote.AllowUserToResizeColumns = false;
+            this.dgvNote.AllowUserToResizeRows = false;
+            this.dgvNote.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNote.Location = new System.Drawing.Point(514, 41);
+            this.dgvNote.Name = "dgvNote";
+            this.dgvNote.ReadOnly = true;
+            this.dgvNote.RowHeadersVisible = false;
+            this.dgvNote.RowHeadersWidth = 51;
+            this.dgvNote.RowTemplate.Height = 24;
+            this.dgvNote.Size = new System.Drawing.Size(423, 320);
+            this.dgvNote.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 553);
+            this.Controls.Add(this.dgvNote);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.TxtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "Form1";
+            this.Text = "YourNote v1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNote)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox TxtMessage;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvNote;
+    }
+}
+
---
+++ noteApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace noteApp
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title");
+            table.Columns.Add("Messages");
+
+            dgvNote.DataSource = table;
+            dgvNote.Columns["Messages"].Visible= false;
+            dgvNote.Columns["Title"].Width = 423;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            TxtMessage.Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, TxtMessage.Text);
+            btnNew.PerformClick();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            int index = dgvNote.CurrentCell.RowIndex;
+            if( index >= -1 )
+             {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                TxtMessage.Text= table.Rows[index].ItemArray[1].ToString();
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = dgvNote.CurrentCell.RowIndex;
+            table.Rows.Clear();
+        }
+    }
+}
